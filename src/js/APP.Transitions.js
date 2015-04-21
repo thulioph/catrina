b739c12104138dfe451511823fdd6d8cb68d7f73
Thulio Philipe <thulioph@gmail.com>
2015-04-21 19:14:30 -0300

    Adiciona transição inicial entre as páginas. - #38

--- src/js/APP.Transitions.js
+++ src/js/APP.Transitions.js
@@ -5,10 +5,31 @@
 var APP = APP || {};
 APP.Transitions = {
   setUp: function() {
-    this.startTransition();
+    this.checkPage();
   },
 
-  startTransition: function() {
-    console.log('Hey!');
+  checkPage:function() {
+    var id, that;
+
+    that = this;
+    id = $('body').attr('id');
+
+    that.activeTransition(id);
+  },
+
+  activeTransition: function(id) {
+    var content;
+
+    content = $(id).smoothState({
+      prefetch: true, // delay between hover and click
+      pageCacheSize: 4, // cache in memory
+      onStart: { // run with click
+        duration: 500, // duration of animations
+        render: function(url, $container) {
+          content.toggleAnimationClass('is-exiting');
+          $('body').animate({ scrollTop: 0 });
+        }
+      }
+    }).data('smoothState');
   }
 }
